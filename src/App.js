cc6501a24915e155d60efec37678990346ce4cce
NadaLabidi1 <nada.labidi@esprit.tn>
2022-01-24 07:44:37 +0100

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div> 
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component= {HomePage} />
+        <Route exact path='/shop' component = {ShopPage} />
+      </Switch>
     </div>
   );
 }
